59a1c4b2f3f8b70cdb4df0fc0563798fbe3bff30
Thomas Clavier <tom@tcweb.org>
2021-06-20 16:30:13 +0200

    Remove CanaryMod ...

--- src/docs/java/jscript.java
+++ src/docs/java/jscript.java
@@ -1,6 +1,8 @@
-import javax.script.*;
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.io.FileReader;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class jscript {
@@ -10,7 +12,6 @@ public class jscript {
         java.io.File file = new java.io.File(args[0]);
         engine.put("engine", engine);
         engine.put("args", args);
-        engine.put("cmItemTypeClass", Class.forName("net.canarymod.api.inventroy.ItemType"));
         Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
         bindings.put("polyglot.js.allowHostAccess", true);
         bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
