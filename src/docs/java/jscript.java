c8bb2d876f5f175e8a00efc7872d0c4fbda953e5
Thomas Clavier <tom@tcweb.org>
2021-06-20 16:15:48 +0200

    Test

--- src/docs/java/jscript.java
+++ src/docs/java/jscript.java
@@ -1,20 +1,19 @@
 import javax.script.*;
 import java.io.FileReader;
-import net.canarymod.api.inventory.ItemType;
+import java.util.List;
+import java.util.function.Predicate;
 
-public class jscript
-{
-    public static void main(String[] args) throws Exception
-    {
+public class jscript {
+    public static void main(String[] args) throws Exception {
         ScriptEngineManager factory = new ScriptEngineManager();
         ScriptEngine engine = factory.getEngineByName("JavaScript");
         java.io.File file = new java.io.File(args[0]);
-        engine.put("engine",engine);
-        engine.put("args",args);
-	try { 
-	    engine.put("cmItemTypeClass",Class.forName("net.canarymod.api.inventroy.ItemType"));
-	}catch(Exception e){
-	}
+        engine.put("engine", engine);
+        engine.put("args", args);
+        engine.put("cmItemTypeClass", Class.forName("net.canarymod.api.inventroy.ItemType"));
+        Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+        bindings.put("polyglot.js.allowHostAccess", true);
+        bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
         FileReader fr = new java.io.FileReader(file);
         engine.eval(fr);
         fr.close();
--- src/main/java/bukkit/org/scriptcraftjs/bukkit/ScriptCraftPlugin.java
+++ src/main/java/bukkit/org/scriptcraftjs/bukkit/ScriptCraftPlugin.java
@@ -4,25 +4,23 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import javax.script.*;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
-public class ScriptCraftPlugin extends JavaPlugin
-{
+public class ScriptCraftPlugin extends JavaPlugin {
     public boolean canary = false;
     public boolean bukkit = true;
+    protected ScriptEngine engine = null;
     // right now all ops share the same JS context/scope
     // need to look at possibly having context/scope per operator
     //protected Map<CommandSender,ScriptCraftEvaluator> playerContexts = new HashMap<CommandSender,ScriptCraftEvaluator>();
     private String NO_JAVASCRIPT_MESSAGE = "No JavaScript Engine available. ScriptCraft will not work without Javascript.";
-    protected ScriptEngine engine = null;
 
-    @Override public void onEnable()
-    {
+    @Override
+    public void onEnable() {
         Thread currentThread = Thread.currentThread();
         ClassLoader previousClassLoader = currentThread.getContextClassLoader();
         currentThread.setContextClassLoader(getClassLoader());
@@ -32,6 +30,9 @@ public class ScriptCraftPlugin extends JavaPlugin
             if (this.engine == null) {
                 this.getLogger().severe(NO_JAVASCRIPT_MESSAGE);
             } else {
+                Bindings bindings = this.engine.getBindings(ScriptContext.ENGINE_SCOPE);
+                bindings.put("polyglot.js.allowHostAccess", true);
+                bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
                 Invocable inv = (Invocable) this.engine;
                 this.engine.eval(new InputStreamReader(this.getResource("boot.js")));
                 inv.invokeFunction("__scboot", this, engine);
@@ -46,15 +47,14 @@ public class ScriptCraftPlugin extends JavaPlugin
 
     public List<String> onTabComplete(CommandSender sender, Command cmd,
                                       String alias,
-                                      String[] args)
-    {
+                                      String[] args) {
         List<String> result = new ArrayList<String>();
         if (this.engine == null) {
             this.getLogger().severe(NO_JAVASCRIPT_MESSAGE);
             return null;
         }
         try {
-            Invocable inv = (Invocable)this.engine;
+            Invocable inv = (Invocable) this.engine;
             inv.invokeFunction("__onTabComplete", result, sender, cmd, alias, args);
         } catch (Exception e) {
             sender.sendMessage(e.getMessage());
@@ -62,9 +62,8 @@ public class ScriptCraftPlugin extends JavaPlugin
         }
         return result;
     }
-    
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-    {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         boolean result = false;
         Object jsResult = null;
         if (this.engine == null) {
@@ -72,14 +71,14 @@ public class ScriptCraftPlugin extends JavaPlugin
             return false;
         }
         try {
-            jsResult = ((Invocable)this.engine).invokeFunction("__onCommand", sender, cmd, label, args);
+            jsResult = ((Invocable) this.engine).invokeFunction("__onCommand", sender, cmd, label, args);
         } catch (Exception se) {
             this.getLogger().severe(se.toString());
             se.printStackTrace();
             sender.sendMessage(se.getMessage());
         }
-        if (jsResult != null){
-            return ((Boolean)jsResult).booleanValue();
+        if (jsResult != null) {
+            return ((Boolean) jsResult).booleanValue();
         }
         return result;
     }
