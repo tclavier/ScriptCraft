29aec2b65f0f36e831441d0e415d19c7bb35e927
Thomas Clavier <tom@tcweb.org>
2021-06-20 16:33:02 +0200

    init

--- src/docs/java/jscript.java
+++ src/docs/java/jscript.java
@@ -12,10 +12,10 @@ public class jscript {
         Bindings bindings = engine.getBindings(ScriptContext.ENGINE_SCOPE);
         bindings.put("polyglot.js.allowHostAccess", true);
         bindings.put("polyglot.js.allowHostClassLookup", (Predicate<String>) s -> true);
-        FileReader fr = new java.io.FileReader(file);
         java.io.File file = new java.io.File(args[0]);
         engine.put("engine", engine);
         engine.put("args", args);
+        FileReader fr = new java.io.FileReader(file);
         engine.eval(fr);
         fr.close();
     }
